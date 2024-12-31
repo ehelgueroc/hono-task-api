8d0d459d61867797920f6cc3c6940a98fc5e2247
Esteban Helguero <e.helguero.c@gmail.com>
2024-12-31 19:45:59 +0000

    updated pino logger config to use pino pretty

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
+import type { PinoLogger } from "hono-pino";
+
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { notFound, onError } from "stoker/middlewares";
 
 import { pinoLoggerMiddleware } from "./middlewares/pino-logger.js";
 
+interface AppBindings {
+  Variables: {
+    logger: PinoLogger;
+  };
+}
+
 // extended hono that supports openapi
-const app = new OpenAPIHono();
+const app = new OpenAPIHono<AppBindings>();
 app.use(pinoLoggerMiddleware());
 
 app.get("/", (c) => {
--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,5 +1,17 @@
 import { pinoLogger } from "hono-pino";
+import { pino } from "pino";
+import pretty from "pino-pretty";
 
 export function pinoLoggerMiddleware() {
-  return pinoLogger();
+  return pinoLogger({
+    // pass pino instance to hono-pino to customize logger
+    pino: pino({
+      level: process.env.LOG_LEVEL || "info",
+    }, process.env.NODE_ENV === "production" ? undefined : pretty()),
+    http: {
+      // make unique request id with uuid
+      // useful for tracing requests across services
+      reqId: () => crypto.randomUUID(),
+    },
+  });
 }
