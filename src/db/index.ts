c03d41b2dd794c53eb8d656aff6dbc8a195c9e39
Esteban Helguero <e.helguero.c@gmail.com>
2025-01-27 22:30:32 +0000

    Revert "config drizzle in api"
    This reverts commit 1664a6bfa4099c3d496d16f2a4fcfa9ca19c427b.

--- src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
-import env from "@/env.js";
-
-const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN });
-const db = drizzle(client);
-export default db;
--- src/routes/tasks/tasks.routes.ts
+++ src/routes/tasks/tasks.routes.ts
@@ -2,15 +2,11 @@ import { createRoute, z } from "@hono/zod-openapi";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
 
-// create route for tasks
-// It should handle GET requests to /tasks
-// and respond with a list of tasks
 export const list = createRoute({
   tags: ["Tasks"],
   method: "get",
   path: "/tasks",
   responses: {
-    // define the response for a successful GET request
     [HttpStatusCodes.OK]: jsonContent(
       z.array(z.object({
         name: z.string(),
