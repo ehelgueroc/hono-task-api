1664a6bfa4099c3d496d16f2a4fcfa9ca19c427b
Esteban Helguero <e.helguero.c@gmail.com>
2025-01-27 22:28:36 +0000

    config drizzle in api

--- src/routes/tasks/tasks.handler.ts
+++ src/routes/tasks/tasks.handler.ts
@@ -2,6 +2,8 @@ import type { AppRouteHandler } from "@/lib/types.js";
 
 import type { TasksRoutes } from "./tasks.routes.js";
 
+// tasks list handle
+// returns a json list of tasks
 export const list: AppRouteHandler<TasksRoutes> = (c) => {
   return c.json([
     { name: "Task 1", done: false },
